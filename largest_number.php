30bd138229ced59e4e7d66d13f9d5ad6fd29eece
rhrana9321 <rhrana12@gmail.com>
2024-07-03 22:14:24 +0600

    i change echo

--- largest_number.php
+++ largest_number.php
@@ -9,6 +9,6 @@ if ($num1 > $num2 && $num1 > $num3) {
 } elseif ($num2 > $num1 && $num2 > $num3) {
     echo "The number $num2 is the largest";
 } else if($num3 > $num1 && $num3 > $num2){
-    echo "The number $num3 is the largest";
+    echo "The number is $num3 is the largest";
 }
 
